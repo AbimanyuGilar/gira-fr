f8a93dded76a0849251df55f796f5d3190d3bb61
AbimanyuGilar <gilarwaluyo@gmail.com>
2025-01-24 11:14:19 +0700

    add the default view when no transactions listed

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -21,54 +21,58 @@
 
     <main class="flex items-center justify-center flex-grow bg-slate-200">
         <div class="grid grid-cols-3 bg-white shadow-xl h-[750px] w-[1500px] rounded-xl p-5 gap-4">
-            <div class="overflow-y-scroll">
-                @foreach ($filteredTransactions as $transaction )
-                    <div class="flex justify-end p-3 my-2 border-2 rounded-md hover:bg-gray-50 border-slate-900 h-28" onclick="event.preventDefault(); document.getElementById('selected').value='{{ $transaction->id }}'; document.getElementById('filter').submit()">
-                        <div class="grid w-full grid-cols-1 rounded-xl">
-                            @if ($transaction->type == 'income')
-                                <p class="text-xl">{{ $transaction->type }}<span class="text-green-700">{{ ' +Rp' . number_format($transaction->amount, 0, ',', '.'); }}</span></p>
-                            @else
-                                <p class="text-xl">{{ $transaction->type }}<span class="text-red-700">{{ ' -Rp' . number_format($transaction->amount, 0, ',', '.'); }}</span></p>
-                            @endif
+            @if (count($filteredTransactions) === 0)
+                <h1>No transaction on this day</h1>
+            @else
+                <div class="overflow-y-scroll">
+                    @foreach ($filteredTransactions as $transaction )
+                        <div class="flex justify-end p-3 my-2 border-2 rounded-md hover:bg-gray-50 border-slate-900 h-28" onclick="event.preventDefault(); document.getElementById('selected').value='{{ $transaction->id }}'; document.getElementById('filter').submit()">
+                            <div class="grid w-full grid-cols-1 rounded-xl">
+                                @if ($transaction->type == 'income')
+                                    <p class="text-xl">{{ $transaction->type }}<span class="text-green-700">{{ ' +Rp' . number_format($transaction->amount, 0, ',', '.'); }}</span></p>
+                                @else
+                                    <p class="text-xl">{{ $transaction->type }}<span class="text-red-700">{{ ' -Rp' . number_format($transaction->amount, 0, ',', '.'); }}</span></p>
+                                @endif
 
-                            <p class="truncate ...">{{ $transaction->description }}</p>
-                        </div>
-                        @php
-                            $options = ["edit", "delete"];
-                        @endphp
-
-                        <div x-data="{ isOpen: false }" class="relative inline-block text-left" onclick="event.stopPropagation()">
-                            <div>
-                              <button @click="isOpen = !isOpen" type="button" class="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-md hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-                                </svg>
-                              </button>
+                                <p class="truncate ...">{{ $transaction->description }}</p>
                             </div>
-                            <div
-                                x-show="isOpen"
-                                x-transition:enter="transition ease-out duration-100 transform"
-                                x-transition:enter-start="opacity-0 scale-95"
-                                x-transition:enter-end="opacity-100 scale-100"
-                                x-transition:leave="transition ease-in duration-75 transform"
-                                x-transition:leave-start="opacity-100 scale-100"
-                                x-transition:leave-end="opacity-0 scale-95"
-                                class="absolute right-0 z-10 w-56 mt-2 overflow-y-scroll origin-top-right bg-white rounded-md shadow-lg max-h-64 ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
-                                @click.away="isOpen = false"
-                            >
-                                <div class="py-1" role="none">
-                                    <a href="{{ route('transactions.edit', $transaction) }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50" role="menuitem" tabindex="-1" id="menu-item-0">Edit</a>
-                                    <form action="{{ route('transactions.destroy', $transaction) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" onclick="return confirm('Are you sure?')" class="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 text-start" role="menuitem" tabindex="-1" id="menu-item-0">Delete</button>
-                                    </form>
+                            @php
+                                $options = ["edit", "delete"];
+                            @endphp
+
+                            <div x-data="{ isOpen: false }" class="relative inline-block text-left" onclick="event.stopPropagation()">
+                                <div>
+                                <button @click="isOpen = !isOpen" type="button" class="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-md hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                                    </svg>
+                                </button>
+                                </div>
+                                <div
+                                    x-show="isOpen"
+                                    x-transition:enter="transition ease-out duration-100 transform"
+                                    x-transition:enter-start="opacity-0 scale-95"
+                                    x-transition:enter-end="opacity-100 scale-100"
+                                    x-transition:leave="transition ease-in duration-75 transform"
+                                    x-transition:leave-start="opacity-100 scale-100"
+                                    x-transition:leave-end="opacity-0 scale-95"
+                                    class="absolute right-0 z-10 w-56 mt-2 overflow-y-scroll origin-top-right bg-white rounded-md shadow-lg max-h-64 ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
+                                    @click.away="isOpen = false"
+                                >
+                                    <div class="py-1" role="none">
+                                        <a href="{{ route('transactions.edit', $transaction) }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50" role="menuitem" tabindex="-1" id="menu-item-0">Edit</a>
+                                        <form action="{{ route('transactions.destroy', $transaction) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" onclick="return confirm('Are you sure?')" class="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 text-start" role="menuitem" tabindex="-1" id="menu-item-0">Delete</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
+            @endif
 
             <div class="grid grid-rows-3 p-10 border-2 border-slate-900 aspect-square rounded-xl">
                 @if ($selectedTransaction)
